b3b78d097deb134e93a39e0c19f0babcd6d531d4
Eric Daff <edaff1994@gmail.com>
2019-02-08 09:29:08 -0500

    Using_Relative_Path

--- src/wavm_mod.cpp
+++ src/wavm_mod.cpp
@@ -12,8 +12,19 @@
 #include <stdlib.h>
 
 std::string wavm_mod(std::string message) {
+	std::string output_file_name = "wavm_output.txt";
+	int result;
+
 	// Message contains the '-wavm' part, so just get the filename
 	std::string filename = message.substr(7);
+
+	// Set the command to be executed by cmd
+	std::string path_command = "cd > wavm_output.txt";
+
+	result = system(path_command.c_str());
+	std::string current_path = retrieve_output_from_file(output_file_name);
+
+	std::cout << current_path << std::endl;
 
 	// Find the path to the WAVM executable
         std::string wavm_exe = "C:/Users/xarga/Desktop/minetest-windows-build/wavm_bin/wavm-run.exe";
@@ -23,16 +34,21 @@ std::string wavm_mod(std::string message) {
 
 	// Run the executable and pipe the output to a text file
         std::string command = wavm_exe + " " + filename + " > wavm_output.txt";
-        int result = system(command.c_str());
+        result = system(command.c_str());
 
-        // Citation: www.cplusplus.com/doc/tutorial/files/
-        // Below code to read from a file was influenced by the sample code provided by the above link
-	// Grab the content of the text file and return it
-        std::string content;
-        std::ifstream output("wavm_output.txt");
-        if(output.is_open())
-                std::getline(output, content);
-        output.close();
+        return retrieve_output_from_file(output_file_name);
+}
 
-        return content;
+// Citation: www.cplusplus.com/doc/tutorial/files/
+// Below code to read from a file was influenced by the sample code provided by the above link
+// Grab the content of the text file and return it
+std::string retrieve_output_from_file(std::string filename) {
+	std::string content;
+	std::ifstream output(filename);
+	if(output.is_open()){
+		std::getline(output, content);
+	}
+	output.close();
+
+	return content;
 }
