3fd669cdcad2bab287ed18dc29079f8e57489597
Eric Daff <edaff1994@gmail.com>
2019-03-27 13:08:17 -0400

    Updated permissions and node_example_index.js

--- wavm_bin/WAST_files/node_example_index.js
+++ wavm_bin/WAST_files/node_example_index.js
@@ -5,6 +5,6 @@ const Wasm_Binary = Node_Example_Module({wasmBinaryFile: 'node_example.wasm'});
 
 Wasm_Binary.onRuntimeInitialized = function() {
     let mem = Wasm_Binary._create_array(2);
-    console.log(Wasm_Binary._multiply_values_in_array(mem));
+    console.log(Wasm_Binary._multiply_values_in_array(mem, 2457, 2));
     Wasm_Binary._destroy_array(mem);
 };
