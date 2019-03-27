30578fba89dff283f0b713d28c713c2f9f518c0b
Eric Daff <edaff1994@gmail.com>
2019-03-27 11:58:24 -0400

    Updated test nodejs files

--- wavm_bin/WAST_files/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const Module = require('./test.js');
-const wasm = Module({wasmBinaryFile: 'test.wasm'});
-wasm.onRuntimeInitialized = function() {
-    console.log(wasm._add(40, 40));
-    let mem = wasm._create(100, 100);
-    wasm._destroy(mem);
-    console.log("Done");
-};
---
+++ wavm_bin/WAST_files/node_example_index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const Node_Example_Module = require('./node_example.js');
+const Wasm_Binary = Node_Example_Module({wasmBinaryFile: 'node_example.wasm'});
+
+Wasm_Binary.onRuntimeInitialized = function() {
+    let mem = Wasm_Binary._create_array(2);
+    console.log(Wasm_Binary._multiply_values_in_array(mem));
+    Wasm_Binary._destroy_array(mem);
+};
