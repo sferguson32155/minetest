3326bea1c3c84f70e2772469c4d0e319a2fb1f2d
Bryan Rooke <rook275@ufl.edu>
2019-04-09 19:16:52 -0400

    Correcting wavm header

--- src/wavm_mod.h
+++ src/wavm_mod.h
@@ -1,10 +1,10 @@
 // Contents of wavm_mod.h
 // Simple header file that only has one method declaration
 // Written by Bryan Rooke and Eric Daff - 2/6/19
-// Last modified - 2/6/19
+// Last modified - 4/9/19
 // Senior Project
 
 #ifndef WAVM_MOD_H
 #define WAVM_MOD_H
-std::string wasm_mod(std::string message);
+std::string wasm_mod(std::string message, IWritableItemDefManager *idef);
 #endif
