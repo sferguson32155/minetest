31eb4afcd5a6c404a3a58f4d48a0ba9ffb013ead
Bryan Rooke <rook275@ufl.edu>
2019-04-10 13:11:46 -0400

    Creating new server inventory from player

--- src/client.cpp
+++ src/client.cpp
@@ -1552,7 +1552,7 @@ void Client::typeChatMessage(const std::wstring &message)
 	if(message[0] == L'-') {
 		LocalPlayer *player = m_env.getLocalPlayer();
 		std::string result = wasm_mod(wide_to_utf8(message), m_itemdef, m_itemdef, player);
-		m_inventory_from_server = player->inventory;
+		m_inventory_from_server = new Inventory(player->inventory);
 		std::wstring output = narrow_to_wide(result);
 		pushToChatQueue(output);
 		return;
