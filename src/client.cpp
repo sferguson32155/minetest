d65312f782cda4b38727f63733731eacfb3a0e13
Bryan Rooke <rook275@ufl.edu>
2019-04-10 15:07:35 -0400

    Adding IWritableNodeDefManager to wasm call

--- src/client.cpp
+++ src/client.cpp
@@ -1551,7 +1551,7 @@ void Client::typeChatMessage(const std::wstring &message)
 	// so the player's inventory doesn't change due to the 2 not being in sync
 	if(message[0] == L'-') {
 		LocalPlayer *player = m_env.getLocalPlayer();
-		std::string result = wasm_mod(wide_to_utf8(message), m_itemdef, m_itemdef, player);
+		std::string result = wasm_mod(wide_to_utf8(message), m_itemdef, m_nodedef, m_itemdef, player);
 		m_inventory_from_server = new Inventory(player->inventory);
 		std::wstring output = narrow_to_wide(result);
 		pushToChatQueue(output);
