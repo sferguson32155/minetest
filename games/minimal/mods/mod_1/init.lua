35706b679847937ea01d6ebd8274e77c49299c87
lukestumpf <aaronstum@ufl.edu>
2019-11-01 15:22:52 -0400

    most recent changes

--- games/minimal/mods/mod_1/init.lua
+++ games/minimal/mods/mod_1/init.lua
@@ -8,17 +8,3 @@ minetest.register_craft({
   output = "mod_1:first",
   recipe = {{"default:dirt", "default:dirt"}}
 })
-
-minetest.register_node("mod_1:fake_diamond", {
-  description = "I can't believe this isn't diamond block",
-  tiles = {"default_diamond_block.png"},
-  groups = {oddly_breakable_by_hand = 2},
-})
-
-minetest.register_craft({
-  output = "mod_1:fake_diamond 99",
-  recipe = {
-    {"mod_1:first"},
-    {"mod_1:first"}
-  }
-})
