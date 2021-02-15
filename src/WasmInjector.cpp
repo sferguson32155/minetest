65c9c138367068e050900656881ee3e1733ff4a2
Logan D.G. Smith <loganda.smith@ufl.edu>
2021-02-15 12:45:42 -0500

    Move mod.js into a central file. User no longer needs to provide it.

--- src/WasmInjector.cpp
+++ src/WasmInjector.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <string>
 
+#include <iostream>
+
 #include "WasmInjector.h"
 
 std::string WasmInjector::get_wasm_bytes(const char* path) {
@@ -50,23 +52,47 @@ bool WasmInjector::write_js(std::vector<std::string> js, const char *path) {
     return true;
 }
 
+std::string WasmInjector::get_containing_directory(const char *path) {
+	std::string stringPath(path);
+	std::size_t delimiter = stringPath.find_last_of("/\\");
+
+	return stringPath.substr(0, delimiter);
+}
+
 bool WasmInjector::inject_wasm(const char *path) {
-    auto js = WasmInjector::get_js_contents(path);
-    int begin, end, line;
-    std::string wasm_path = "";
-    for (int i = 0; i < js.size(); i++) {
-        begin = js[i].find("%wb");
-        end = js[i].find("wb%");
-        if (begin != std::string::npos) {
-            line = i;
-            wasm_path = js[i].substr(begin+3, end-begin-3);
-            break;
-        }
-    }
-    if (wasm_path == "") return false;
+    // Path to the target directory to inject
+	std::string js_path(path);
+	js_path += "\\mod.js";
 
-    auto wasm_bytes = WasmInjector::get_wasm_bytes(wasm_path.c_str());
-    js[line] = js[line].substr(0, begin) + wasm_bytes + js[line].substr(end+3, js[line].length()-end);
+    // Get the path to the model file
+    std::string modelJS_path = get_containing_directory(path) + "\\mod.js";
+	std::cout << modelJS_path << std::endl;
 
-    return write_js(js, path);
+    // Copy the model file to the target directory
+	std::cout << "Opening Streams..." << std::endl;
+	{
+		std::ifstream modModelJS(modelJS_path, std::ios::binary);
+		std::ofstream wasmModJS(js_path, std::ios::binary);
+
+		std::cout << "Copying Files..." << std::endl;
+		wasmModJS << modModelJS.rdbuf();
+	}
+
+	// Read the file contents to a string
+    std::cout << "Reading JS..." << std::endl;
+	auto js = WasmInjector::get_js_contents(js_path.c_str());
+
+    // Read the WASM bytes
+	std::cout << "Reading WASM..." << std::endl;
+	std::string wasm_path(path);
+	wasm_path += "\\mod.wasm";
+	auto wasm_bytes = WasmInjector::get_wasm_bytes(wasm_path.c_str());
+
+	// Write the WASM to a buffer. This is line 9, an empty line
+	std::cout << "Injecting..." << std::endl;
+	js[8] = "var buffer = new Uint8Array(" + wasm_bytes + ")";
+
+    // Write to the JS File
+	std::cout << "Writing to File..." << std::endl;
+	return write_js(js, js_path.c_str());
 }
--- src/WasmInjector.h
+++ src/WasmInjector.h
@@ -7,6 +7,7 @@ class WasmInjector {
         static std::string get_wasm_bytes(const char* path);
         static std::vector<std::string> get_js_contents(const char* path);
         static bool write_js(std::vector<std::string> js, const char *path);
+	    static std::string get_containing_directory(const char *path);
     public:
         static bool inject_wasm(const char *path);
 };
