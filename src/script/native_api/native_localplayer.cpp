b07d23e7837464cbc615b7b5a1557500f2f400c3
Lisa-Touchton-DEV <ldtouchton@gmail.com>
2023-02-26 21:13:19 -0500

    WIP localplayer

---
+++ src/script/native_api/native_localplayer.cpp
@@ -0,0 +1,179 @@
+#include "native_localplayer.h"
+
+v3f NativeLocalPlayer::native_get_velocity(LocalPlayer *p)
+{
+	return p->getSpeed();
+}
+
+int NativeLocalPlayer::native_get_hp(LocalPlayer *p)
+{
+	return p->hp;
+}
+
+std::string NativeLocalPlayer::native_get_name(LocalPlayer *p)
+{
+	return p->getName();
+}
+
+u16 NativeLocalPlayer::native_get_wield_index(LocalPlayer *p)
+{
+	return p->getWieldIndex();
+}
+
+ItemStack NativeLocalPlayer::native_get_wielded_item(LocalPlayer *p)
+{
+	ItemStack selected_item;
+	return p->getWieldedItem(&selected_item, nullptr);
+}
+
+bool NativeLocalPlayer::native_is_attached(LocalPlayer *p)
+{
+	return (p->getParent() != nullptr);
+}
+
+bool NativeLocalPlayer::native_is_touching_ground(LocalPlayer *p)
+{
+	return p->touching_ground;
+}
+
+bool NativeLocalPlayer::native_is_in_liquid(LocalPlayer *p)
+{
+	return p->in_liquid;
+}
+
+bool NativeLocalPlayer::native_is_in_liquid_stable(LocalPlayer *p)
+{
+	return p->in_liquid_stable;
+}
+
+int NativeLocalPlayer::native_get_liquid_viscosity(LocalPlayer *p)
+{
+	return p->in_liquid_stable;
+}
+
+bool NativeLocalPlayer::native_is_climbing(LocalPlayer *p)
+{
+	return p->is_climbing;
+}
+
+bool NativeLocalPlayer::native_swimming_vertical(LocalPlayer *p)
+{
+	return p->swimming_vertical;
+}
+
+std::vector<int> NativeLocalPlayer::native_get_physics_override(LocalPlayer *p)
+{
+	std::vector<int> v;
+	v.push_back(p->physics_override_speed);
+	v.push_back(p->physics_override_jump);
+	v.push_back(p->physics_override_gravity);
+	v.push_back(p->physics_override_sneak);
+	v.push_back(p->physics_override_sneak_glitch);
+	v.push_back(p->physics_override_new_move);
+	return v;
+}
+
+v3f NativeLocalPlayer::native_get_last_pos(LocalPlayer *p)
+{
+	return p->last_position;
+}
+
+v3f NativeLocalPlayer::native_get_last_velocity(LocalPlayer *p)
+{
+	return p->last_speed;
+}
+
+//RETURN to this, may be issue
+
+int NativeLocalPlayer::native_get_last_look_vertical(LocalPlayer *p)
+{
+	return -1.0 * p->last_pitch * core::DEGTORAD;
+}
+
+int NativeLocalPlayer::native_get_last_look_horizontal(LocalPlayer *p)
+{
+	return (p->last_yaw + 90.) * core::DEGTORAD;
+}
+
+//Return --unsure 
+
+const PlayerControl& NativeLocalPlayer::native_get_control(LocalPlayer *p)
+{
+	return p->getPlayerControl();
+}
+
+int NativeLocalPlayer::native_get_breath(LocalPlayer *p)
+{
+	return p->getBreath();
+}
+
+v3f NativeLocalPlayer::native_get_pos(LocalPlayer *p)
+{
+	return p->getPosition();
+}
+
+std::vector<int> NativeLocalPlayer::native_get_movement_acceleration(
+		LocalPlayer *p)
+{
+	std::vector<int> movA;
+	movA.push_back(p->movement_acceleration_default);
+	movA.push_back(p->movement_acceleration_air);
+	movA.push_back(p->movement_acceleration_fast);
+
+	return movA;
+}
+std::vector<int> NativeLocalPlayer::native_get_movement_speed(LocalPlayer *p)
+{
+	std::vector<int> movS;
+	
+	movS.push_back(p->movement_speed_walk);
+	movS.push_back(p->movement_speed_crouch);
+	movS.push_back(p->movement_speed_fast);
+	movS.push_back(p->movement_speed_climb);
+	movS.push_back(p->movement_speed_jump);
+
+	return movS;
+}
+
+std::vector<int> NativeLocalPlayer::native_get_movement(LocalPlayer *p)
+{
+	std::vector<int> mov;
+
+	mov.push_back(p->movement_liquid_fluidity);
+	mov.push_back(p->movement_liquid_fluidity_smooth);
+	mov.push_back(p->movement_liquid_sink);
+	mov.push_back(p->movement_speed_climb);
+	mov.push_back(p->movement_gravity);
+
+	return mov;
+}
+
+ItemGroupList NativeLocalPlayer::native_get_armour_groups(LocalPlayer *p)
+{
+	return p->getCAO()->getGroups();
+}
+
+u32 NativeLocalPlayer::native_hud_add(LocalPlayer *p, HudElement *elem)
+{
+	return p->addHud(elem);
+}
+
+HudElement *NativeLocalPlayer::native_hud_remove(LocalPlayer *p, HudElement *elem, u32 id)
+{
+	return p->removeHud(id);
+}
+
+HudElement *NativeLocalPlayer::native_hud_change(LocalPlayer *p, u32 id)
+{
+	HudElement *e = p->getHud(id);
+	if (!e) {
+		return e;
+	} else
+		return nullptr;
+}
+
+HudElement *NativeLocalPlayer::native_hud_get(LocalPlayer *p, u32 id)
+{
+	HudElement *e = p->getHud(id);
+	return e;
+}
---
+++ src/script/native_api/native_localplayer.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "client/localplayer.h"
+#include "../lua_api/l_item.h"
+#include "../../../src/player.h"
+#include "../../../src/irrlicht_changes/irrUString.h"
+#include "../../irr_v3d.h"
+#include "../../../src/client/content_cao.h"
+#include <vector>
+
+
+class NativeLocalPlayer
+{
+public:
+	static v3f native_get_velocity(LocalPlayer *p);
+	static int native_get_hp(LocalPlayer *p);
+	static std::string native_get_name(LocalPlayer *p);
+	static u16 native_get_wield_index(LocalPlayer *p);
+	static ItemStack native_get_wielded_item(LocalPlayer *p);
+
+	static bool native_is_attached(LocalPlayer *p);
+	static bool native_is_touching_ground(LocalPlayer *p);
+	static bool native_is_in_liquid(LocalPlayer *p);
+	static bool native_is_in_liquid_stable(LocalPlayer *p);
+	static int native_get_liquid_viscosity(LocalPlayer *p);
+
+	static bool native_is_climbing(LocalPlayer *p);
+	static bool native_swimming_vertical(LocalPlayer *p);
+	static std::vector<int> native_get_physics_override(LocalPlayer *p);
+	static v3f native_get_last_pos(LocalPlayer *p);
+	static v3f native_get_last_velocity(LocalPlayer *p);
+
+	static int native_get_last_look_vertical(LocalPlayer *p);
+	static int native_get_last_look_horizontal(LocalPlayer *p);
+	static const PlayerControl &native_get_control(LocalPlayer *p);
+	static int native_get_breath(LocalPlayer *p);
+	static v3f native_get_pos(LocalPlayer *p);
+
+	static std::vector<int> native_get_movement_acceleration(LocalPlayer *p);
+	static std::vector<int> native_get_movement_speed(LocalPlayer *p);
+	static std::vector<int> native_get_movement(LocalPlayer *p);
+	static ItemGroupList native_get_armour_groups(LocalPlayer *p);
+	static u32 native_hud_add(LocalPlayer *p, HudElement *elem);
+
+	static HudElement *native_hud_remove(LocalPlayer *p, HudElement *element, u32 id);
+	static HudElement *native_hud_change(LocalPlayer *p, u32 id);
+	static HudElement *native_hud_get(LocalPlayer *p, u32 id);
+};
