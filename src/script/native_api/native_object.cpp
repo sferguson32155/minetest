84703d5324e1cf69de10b8bf2a6462744fd081b5
sferguson32155 <103959194+sferguson32155@users.noreply.github.com>
2023-08-14 13:43:28 -0400

    Update native_object.cpp 6-17
    6/17/2023
    
    Created outlines for:
    
    l_punch
    created:
    n_punch
    l_native_punch
    
    l_right_click
    created:
    n_right_click
    l_native_right_click
    This one does not seem like there is any non-dependent functions avaliable
    
    l_set_hp
    created:
    n_set_hp
    l_native_set_hp
    
    l_get_hp
    created:
    n_get_hp
    l_native_get_hp
    
    l_get_inventory
    created:
    n_get_inventory
    l_native_get_inventory

--- src/script/native_api/native_object.cpp
+++ src/script/native_api/native_object.cpp
@@ -25,3 +25,54 @@ void nativeObjectRef::n_move_to(ServerActiveObject *sao, const v3f &pos, bool co
     sao->moveTo(pos, continuous);
 }
 
+//6-17
+float nativeObjectRef::n_punch(ServerActiveObject *sao, ServerActiveObject *puncher, float time_from_last_punch, const ToolCapabilities &toolcap, const v3f &dir) {
+    dir.normalize();
+    u16 src_original_hp = sao->getHP();
+    u16 dst_origin_hp = puncher->getHP();
+
+    u16 wear = sao->punch(dir, &toolcap, puncher, time_from_last_punch);
+
+    // If the punched is a player, and its HP changed
+    if (src_original_hp != sao->getHP() && sao->getType() == ACTIVEOBJECT_TYPE_PLAYER) {
+        getServer(L)->SendPlayerHPOrDie((PlayerSAO *)sao, PlayerHPChangeReason(PlayerHPChangeReason::PLAYER_PUNCH, puncher));
+    }
+
+    // If the puncher is a player, and its HP changed
+    if (dst_origin_hp != puncher->getHP() && puncher->getType() == ACTIVEOBJECT_TYPE_PLAYER) {
+        getServer(L)->SendPlayerHPOrDie((PlayerSAO *)puncher, PlayerHPChangeReason(PlayerHPChangeReason::PLAYER_PUNCH, sao));
+    }
+
+    return wear;
+}
+
+//Might not be changable
+void nativeObjectRef::n_right_click(ServerActiveObject *sao, ServerActiveObject *sao2) {
+    sao->rightClick(sao2);
+}
+
+void nativeObjectRef::n_set_hp(ServerActiveObject *sao, int hp, const PlayerHPChangeReason& reason) {
+    sao->setHP(hp, reason);
+    if (sao->getType() == ACTIVEOBJECT_TYPE_PLAYER)
+        getServer(L)->SendPlayerHPOrDie((PlayerSAO *)sao, reason);
+    if (reason.hasLuaReference())
+        luaL_unref(L, LUA_REGISTRYINDEX, reason.lua_reference);
+}
+
+int nativeObjectRef::n_get_hp(ServerActiveObject *sao) {
+    if (sao == nullptr) {
+        // Default hp is 1
+        return 1;
+    }
+
+    return sao->getHP();
+}
+
+InvRef nativeObjectRef::n_get_inventory(ServerActiveObject *sao)
+{
+	InventoryLocation loc = sao->getInventoryLocation();
+	if (getServerInventoryMgr(sao->getEnv())->getInventory(loc) != nullptr)
+		return InvRef::create(sao->getEnv(), loc);
+	else
+		return InvRef();
+}
