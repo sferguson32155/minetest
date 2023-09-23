ae89615a595dfb6bf1910473e022400c7e8920d6
sferguson32155 <103959194+sferguson32155@users.noreply.github.com>
2023-09-22 20:45:46 -0400

    Update object.lua

--- mods/testingnativeapi_server/object.lua
+++ mods/testingnativeapi_server/object.lua
@@ -1,6 +1,39 @@
 -- Written By Sean Ferguson
 minetest.log("object_test_obj")
 
+minetest.register_chatcommand("lua_object", {
+    description = "List all avaliable chat commands",
+    func = function(name, param)
+        if param == "" then
+            minetest.log("Enter a page number - /lua_object #")
+        end
+        if param == "1" then
+            minetest.log("lua_object_remove\n")
+            minetest.log("lua_object_get_pos\n")
+            minetest.log("lua_object_set_pos #\n")
+            minetest.log("lua_object_move_to #\n")
+            minetest.log("lua_object_punch - Not Working\n")
+            minetest.log("lua_object_right_click\n")
+            minetest.log("lua_object_set_hp - Not Working\n")
+            minetest.log("lua_object_get_hp\n")
+            minetest.log("lua_object_get_inventory\n")
+            minetest.log("lua_object_get_weild_list\n")
+        end
+        if param == "2" then
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+            minetest.log("")
+        end
+    end,
+})
+
 -- l_remove
 minetest.register_chatcommand("lua_object_remove", {
     description = "Test Object Remove",
@@ -336,5 +369,133 @@ minetest.register_chatcommand("native_object_punch", {
                 end
             end
         end
+    end,
+})
+
+-- l_right_click
+-- right_click(self, clicker)
+minetest.register_chatcommand("lua_object_right_click", {
+    description = "Test Object Right Click",
+    func = function(name, param)
+        minetest.log("lua_object_right_click is running!")
+        local player = minetest.get_player_by_name(name)
+        
+        if not player then
+            minetest.log("Player not found")
+            return
+        end
+
+        local pos = player:get_pos()
+        local saos = minetest.get_objects_inside_radius(pos, 2)
+        
+        -- Check if saos is empty
+        if #saos == 0 then
+            minetest.log("No Active Objects near Player")
+            return
+        end
+        
+        -- Output the size of saos to minetest.log
+        minetest.log("Size of Active Objects Array: " .. #saos)
+
+        -- Insert Lua code here that if saos == 2 then set plyr = saos[0] and rclk = saos[1]
+        if #saos == 2 then
+            local plyr = saos[1]
+            local rclk = saos[2]
+            plyr:right_click(rclk)
+            minetest.log("Object: "..plyr:get_player_name().." right clicked on ".. rclk:get_entity_name())
+        end
+    end,
+})
+
+--set_hp
+
+--get_hp
+-- get_hp(self)
+minetest.register_chatcommand("lua_object_get_hp", {
+    description = "Get the player's current HP",
+    func = function(name, param)
+        minetest.log("lua_object_get_hp is running!")
+        local player = minetest.get_player_by_name(name)
+        
+        if not player then
+            minetest.log("Player not found")
+            return
+        end
+
+        local pos = player:get_pos()
+        local saos = minetest.get_objects_inside_radius(pos, 2)
+        
+        -- Check if saos is empty
+        if #saos == 0 then
+            minetest.log("No Active Objects near Player")
+            return
+        end
+        
+        -- Output the size of saos to minetest.log
+        minetest.log("Size of Active Objects Array: " .. #saos)
+        for i, object in ipairs(saos) do
+            local a = object:get_hp()
+            minetest.log("The object's HP is currently " ..dump(a))
+        end
+    end,
+})
+
+-- get_inventory(self)
+minetest.register_chatcommand("lua_object_get_inventory", {
+    description = "Check player's inventory and returns inventory functions",
+    func = function(name, param)
+        minetest.log("lua_object_get_inventory is running!")
+        local player = minetest.get_player_by_name(name)
+        
+        if not player then
+            minetest.log("Player not found")
+            return
+        end
+
+        local pos = player:get_pos()
+        local saos = minetest.get_objects_inside_radius(pos, 2)
+        
+        -- Check if saos is empty
+        if #saos == 0 then
+            minetest.log("No Active Objects near Player")
+            return
+        end
+        
+        -- Output the size of saos to minetest.log
+        minetest.log("Size of Active Objects Array: " .. #saos)
+
+        local plyr = saos[1]
+        local a = plyr:get_inventory()
+        minetest.log(dump(a:get_list("main")))
+    end,
+})
+
+-- get_weild_list(self)
+minetest.register_chatcommand("lua_object_get_weild_list", {
+    description = "Get the player's current HP",
+    func = function(name, param)
+        minetest.log("lua_object_get_weild_list is running!")
+        local player = minetest.get_player_by_name(name)
+        
+        if not player then
+            minetest.log("Player not found")
+            return
+        end
+
+        local pos = player:get_pos()
+        local saos = minetest.get_objects_inside_radius(pos, 2)
+        
+        -- Check if saos is empty
+        if #saos == 0 then
+            minetest.log("No Active Objects near Player")
+            return
+        end
+        
+        -- Output the size of saos to minetest.log
+        minetest.log("Size of Active Objects Array: " .. #saos)
+
+        local plyr = saos[1]
+        local a = plyr:get_wield_list()
+        minetest.log("The player's wield list is " ..dump(a))
     end,
 })
